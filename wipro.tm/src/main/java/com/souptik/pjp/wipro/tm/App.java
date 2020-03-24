77c7389c4b06aa7c75781a5e3f343272b1812b2f
Souph <souptik43.hari@gmail.com>
2020-03-24 21:33:12 +0530

    initial commit

---
+++ wipro.tm/src/main/java/com/souptik/pjp/wipro/tm/App.java
@@ -0,0 +1,13 @@
+package com.souptik.pjp.wipro.tm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
